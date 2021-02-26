0d8659a27afc6a0ccf0f01a6b4dc2bc48e106a68
Jared Wu <Jared_Wu@compal.com>
2021-02-26 14:57:09 +0800

    Update portal page

--- manager/__init__.py
+++ manager/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-# from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 # from flask_wtf.csrf import CSRFProtect
@@ -21,14 +20,6 @@ def create_app():
 
     db.init_app(app)
     bcrypt.init_app(app)
-    # login_manager.init_app(app)
-    # login_manager.login_view = 'auth_service.user_login'
-    # login_manager.blueprint_login_views = {
-    #     'portal': 'portal.login_page',
-    #     'backstage': 'portal.login_page'
-    # }
-    # login_manager.login_message = 'Please login to access this website'
-    # login_manager.login_message_category = 'info'
     jwt.init_app(app)
     cors = CORS(app, resources={r"*": {"origins": "*"}})
 
@@ -36,14 +27,12 @@ def create_app():
     from manager.list_service.list_service import list_service
     from manager.web_portal.web_portal import portal
     from manager.auth.auth_service import auth_service
-    from manager.backstage.backstage import backstage
     from manager.models import User
     from manager.streaming.streaming_service import streaming_service
     app.register_blueprint(main)
     app.register_blueprint(list_service)
     app.register_blueprint(portal, url_prefix='/portal')
     app.register_blueprint(auth_service)
-    app.register_blueprint(backstage, url_prefix='/backstage')
     app.register_blueprint(streaming_service, url_prefix='/streaming')
     return app
 
